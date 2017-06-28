20bf004a0b1244865a3f26afdd9273eb6fc5bc48
Qiuncheng <qiuncheng@gmail.com>
2017-06-28 11:10:19 +0800

    Update for tongzhuogame login.

--- YLLoginManager/YLLoginManager/YLAuthManager-Bridging-Header.h
+++ YLLoginManager/YLLoginManager/YLAuthManager-Bridging-Header.h
@@ -3,21 +3,7 @@
 //
 
 #import "WXApi.h"
-
-#if defined(__has_include)
-
-#if __has_include(<TencentOpenAPI/TencentOAuth.h>)
-#include <TencentOpenAPI/TencentOAuth.h>
-#endif
-
-#if __has_include(<TencentOpenAPI/QQApiInterfaceObject.h>)
-#include <TencentOpenAPI/QQApiInterfaceObject.h>
-#endif
-
-#if __has_include(<TencentOpenAPI/QQApiInterface.h>)
-#include <TencentOpenAPI/QQApiInterface.h>
-#endif
-
 #import "WeiboSDK.h"
-
-#endif
+#include <TencentOpenAPI/QQApiInterfaceObject.h>
+#include <TencentOpenAPI/QQApiInterface.h>
+#include <TencentOpenAPI/TencentOAuth.h>
